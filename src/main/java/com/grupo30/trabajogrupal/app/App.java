6696b4af0a5473b299d71face6479a9d60b4a93e
gonza <gonzagiacomino18@gmail.com>
2024-09-12 11:39:10 -0300

    Se acomodó la app con los métodos respectivos

--- src/main/java/com/grupo30/trabajogrupal/app/App.java
+++ src/main/java/com/grupo30/trabajogrupal/app/App.java
@@ -2,6 +2,8 @@ package com.grupo30.trabajogrupal.app;
 
 
 import com.grupo30.trabajogrupal.dto.*;
+import com.grupo30.trabajogrupal.factory.CreateTables;
+import com.grupo30.trabajogrupal.dao.*;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -9,21 +11,20 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) throws SQLException {
-        /*CREACION DE TABLAS E INSERCIÓN DE ELEMENTOS
         CreateTables tablas = new CreateTables();
         tablas.createTables();
 
         ClienteImpl clienteDAO = new ClienteImpl();
-        clienteDAO.insertClientes();
+        clienteDAO.insertAll();
 
         ProductosImpl productosDAO = new ProductosImpl();
-        productosDAO.insertProductos();
+        productosDAO.insertAll();
 
         FacturasImpl facturasDAO = new FacturasImpl();
-        facturasDAO.insertFacturas();
+        facturasDAO.insertAll();
 
         FacturasProductosImpl facProdDAO = new FacturasProductosImpl();
-        facProdDAO.insertFacturasProductos();*/
+        facProdDAO.insertAll();
 
         Servicios servicios = new Servicios();
 
